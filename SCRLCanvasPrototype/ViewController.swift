21131a89884e5570fbe3a4a763b749ebc50be246
Sergey Slobodenyuk <sergn75@gmail.com>
2025-03-11 16:42:19 +0100

    Initial Commit

---
+++ SCRLCanvasPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCRLCanvasPrototype
+//
+//  Created by Sergey Slobodenyuk on 2025-03-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
